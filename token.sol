4b360fd4a6713cc1d25d94da3c087b853c4e31a1
Imdavyking <63329343+Imdavyking@users.noreply.github.com>
2023-01-27 03:18:10 +0100

    Update token.sol

--- token.sol
+++ token.sol
@@ -10,22 +10,41 @@ contract CoinPocket is ERC20 {
     address public owner;
     mapping (address => uint256) private investments;
     mapping (address => uint256) private lastInvestTime;
-    uint256 public ROI;
-    uint256 public withdrawDuration = 6 days; // duration in seconds
+    mapping (address => uint256) private ROI;
+    mapping (address => uint256) private withdrawDuration;
 
     function invest() public payable {
         lastInvestTime[msg.sender] = block.timestamp;
         investments[msg.sender] += msg.value;
+
+        if (msg.value >= 50000) {
+            ROI[msg.sender] = 5;
+            withdrawDuration[msg.sender] = 50 days;
+        } else if (msg.value >= 20000) {
+            ROI[msg.sender] = 4;
+            withdrawDuration[msg.sender] = 6 days;
+        } else if (msg.value >= 5000) {
+            ROI[msg.sender] = 3;
+            withdrawDuration[msg.sender] = 6 days;
+        } else if (msg.value >= 300) {
+            ROI[msg.sender] = 2.5;
+            withdrawDuration[msg.sender] = 6 days;
+        }
     }
 
+
     function withdraw() public {
-        require(block.timestamp >= lastInvestTime[msg.sender] + withdrawDuration, "Duration not yet completed");
+        require(block.timestamp >= lastInvestTime[msg.sender] + withdrawDuration[msg.sender], "Duration not yet completed");
         require(investments[msg.sender] > 0, "No investments to withdraw");
 
         address user = msg.sender;
-        uint256 amount = investments[user] * ROI;
+        uint256 amount = investments[user] * ROI[msg.sender];
         _transfer(address(this), user, amount);
         investments[user] = 0;
+    }
+
+    function getProfit() public view returns (uint256) {
+        return investments[msg.sender] * ROI[msg.sender];
     }
     
       function withdrawAllEther() public {
@@ -34,10 +53,6 @@ contract CoinPocket is ERC20 {
 
         // Send all ether held by the contract to the owner's address
         msg.sender.transfer(address(this).balance);
-    }
-
-    function getProfit() public view returns (uint256) {
-        return investments[msg.sender] * ROI;
     }
     
     function receive() external payable {
